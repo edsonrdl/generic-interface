53322dcd38c7bf38ba5b661c752f7bb4c2e0b8a7
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2025-09-22 19:46:02 -0300

    feat; criado componente top bar

---
+++ src/app/shared/components/top-bar/top-bar.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component,signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { StyleClassModule  } from 'primeng/styleclass';
+import { ConfiguratorComponent } from '../../components/configurator/configurator.component';
+import { LayoutService } from '../../components/service/layout.service';
+
+@Component({
+  selector: 'app-top-bar',
+    standalone: true,
+    imports: [RouterModule,CommonModule,StyleClassModule ,ConfiguratorComponent],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.scss'
+})
+export class TopBarComponent {
+
+  items!: MenuItem[];
+
+    constructor(public layoutService: LayoutService) {}
+
+    toggleDarkMode() {
+        this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
+    }
+
+}
