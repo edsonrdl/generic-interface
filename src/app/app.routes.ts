86851a0ade2af3125eb0c71e48792071d403a1ab
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2025-09-22 20:08:13 -0300

    feat: criado estrutura

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './shared/components/layout/layout.component';
+import { ToDoComponent } from './features/to-do/to-do.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            { path: '', component: ToDoComponent },
+
+        ]
+    },
+       { path: '**', redirectTo: '/notfound' }
+
+];
